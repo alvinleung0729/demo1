7ae18b1780de5dc3370c6b8537a51ee08a7b00a9
alvinleung0729 <alvinleung0729@gmail.com>
2025-11-21 09:17:24 +0800

    Update 21.11.2025_demo.py

--- 21.11.2025_demo.py
+++ 21.11.2025_demo.py
@@ -3,5 +3,5 @@ import streamlit as st
 
 st.title("Business Dashboard with Streamlit Layouts")
 
-msg = "## Objective: To demonstrate the usage of columns, tabs, and dynamic containers in a business dashboard."
+msg = "## Objective: To demonstrate the usage of columns, tabs, and dynamic containers in a business dashboard." # '##' means header 2 --> 'markdown' --> different font size
 st.write(msg)
