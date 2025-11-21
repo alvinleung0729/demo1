d5d8831aa6d37fe036b1c2af118a98fb196486fb
alvinleung0729 <alvinleung0729@gmail.com>
2025-11-21 09:21:14 +0800

    Update 21.11.2025_demo.py

--- 21.11.2025_demo.py
+++ 21.11.2025_demo.py
@@ -5,3 +5,14 @@ st.title("Business Dashboard with Streamlit Layouts")
 
 msg = "## Objective: To demonstrate the usage of columns, tabs, and dynamic containers in a business dashboard." # '##' means header 2 --> 'markdown' --> different font size
 st.write(msg)
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.header("Q1 2024")
+    st.write("Revenue: $1.2M")
+with col2:
+    st.header("Q2 2024")
+    st.write("Revenue: $1.5M")
+with col3:
+    st.header("Q3 2024")
+    st.write("Revenue: $1.3M")
