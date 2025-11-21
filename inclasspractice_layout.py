d2628bf4f9c2ead627cd8e65436bf29ab6a219ac
alvinleung0729 <alvinleung0729@gmail.com>
2025-11-21 10:02:35 +0800

    Create inclasspractice_layout.py

---
+++ inclasspractice_layout.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+# 1. Dashboard Title and Objective
+# TODO: Add title and description
+st.header("Business Performance Dashboard")
+
+# 2. Columns Layout
+# TODO: Display Q1, Q2, Q3 revenue in columns
+
+# 3. Tabs
+# TODO: Create tabs for Sales Data, Customer Insights, Market Trends
+
+# 4. Expander
+# TODO: Add expander for additional info
+
+# 5. Interactivity
+# TODO: Add selectbox and slider for revenue adjustment
+
+# 6. Bonus
+# TODO: Add bar chart and motivational button
